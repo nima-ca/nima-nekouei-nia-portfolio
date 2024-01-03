9b4c26bbc0f766301be8a929bf2fc7e7af415d72
Nima Nekouei Nia <nekoueinima@gmail.com>
2024-01-04 01:41:18 +0330

    feat: add favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,25 @@ import { FC } from "react";
 const DocumentPage: FC = () => {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/favicons/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicons/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicons/favicon-16x16.png"
+                />
+            </Head>
             <body className="dark:bg-neutral-900 light:bg-neutral-50">
                 <Main />
                 <NextScript />
