c70867e54d8c8932541e89ce523e13f9a747be69
Nima Nekouei Nia <nekoueinima@gmail.com>
2024-01-04 19:11:07 +0330

    feat: change scripts location to _app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import "@src/styles/globals.scss";
 import { NextPageWithLayout } from "@src/types/page";
 import type { AppProps } from "next/app";
 import { Montserrat } from "next/font/google";
+import Script from "next/script";
 import { FC } from "react";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
@@ -14,15 +15,33 @@ interface AppPropsWithLayout extends AppProps {
 
 const App: FC<AppPropsWithLayout> = ({ Component, pageProps }) => {
     const getLayout = Component.getLayout ?? ((page) => page);
+    const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
     return (
-        <Providers>
-            <main
-                className={`${montserrat.className} flex w-full flex-col lg:flex-row`}
-            >
-                <SideNav />
-                {getLayout(<Component {...pageProps} />)}
-            </main>
-        </Providers>
+        <>
+            <Script
+                strategy="lazyOnload"
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+            />
+
+            <Script id="google-analytics" strategy="lazyOnload">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GA_MEASUREMENT_ID}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+            </Script>
+            <Providers>
+                <main
+                    className={`${montserrat.className} flex w-full flex-col lg:flex-row`}
+                >
+                    <SideNav />
+                    {getLayout(<Component {...pageProps} />)}
+                </main>
+            </Providers>
+        </>
     );
 };
 
